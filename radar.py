213145bb26daa97722f3aa290da598cfcf561665
zmpeg <zandstra.matt@gmail.com>
2016-07-07 13:56:02 -0600

    get yesterday urls, add sample regex to clarify purpose

--- radar.py
+++ radar.py
@@ -12,13 +12,14 @@ load_dotenv(find_dotenv())
 def lambda_handler(event, context):
     bucket = os.environ['BUCKET']
     t = parse(event['time'])
-    morning_urls = get_image_urls('NAT', t.year, t.month, t.day, '00', '00', 12)
-    evening_urls = get_image_urls('NAT', t.year, t.month, t.day, '12', '00', 12)
+    morning_urls = get_image_urls('NAT', t.year, t.month, t.day-1, '00', '00', 12)
+    evening_urls = get_image_urls('NAT', t.year, t.month, t.day-1, '12', '00', 12)
     return transfer_images(morning_urls + evening_urls, bucket)
 
 def transfer_images(images, bucket):
     uploads = []
     for image in images:
+        # regex operates on: "/lib/radar/image.php?time=01-JAN-16+12.20.46.889824+AM&site=NAT"
         results = re.search(r'image\.php\?time=([0-9]{2})-([A-Z]*)-([0-9]{2})\+([0-9]{2})\.([0-9]{2})\.([0-9]{2}).*$', image)
         day, month, year, hour, minute = (results.group(1), results.group(2), results.group(3), results.group(4), results.group(5))
         result = s3.Object('weather-radar', 'radar-%(year)s-%(month)s-%(day)s_%(hour)s.%(minute)s.gif' % locals()).put(Body=urllib2.urlopen('http://climate.weather.gc.ca%(image)s' % locals(), 'rb').read())
