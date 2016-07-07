bd0f14906c89fa8d194e361685634c1b1940e47e
zmpeg <zandstra.matt@gmail.com>
2016-07-07 11:38:44 -0600

    Implement function to grab last 24hr of URLs

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 AWS Lambda function to fetch radar images periodically from weather.gc.ca
 
-__endpoint__
-http://climate.weather.gc.ca/radar/index_e.html?site=NAT&year=2016&month=7&day=6&hour=13&minute=50&duration=2&image_type=PRECIPET_RAIN_WEATHEROFFICE
-HTML markup from that endpoint contains js definition `blobArray` which has the image URLs we 
-need to fetch.
+## Install
 
+1. Create a lambda function through the AWS Console.
+2. Configure the lambda function to run daily.
+3. Run the deploy script in this repository: `./deploy.sh`
 
+Thats it! The lambda function will now run daily and grab the last 24 URLs. It does not yet actually download the images to AWS S3.
--- radar.py
+++ radar.py
@@ -1,6 +1,31 @@
-def lambda_handler(event, context):
-    message = 'Hello World'
+import urllib2
+import re
+from lib.BeautifulSoup import BeautifulSoup
+from dateutil.parser import parse
 
-    return {
-        'message' : message
-    }
+def lambda_handler(event, context):
+    t = parse(event['time'])
+    morning_urls = get_image_urls('NAT', t.year, t.month, t.day, '00', '00', 12)
+    evening_urls = get_image_urls('NAT', t.year, t.month, t.day, '12', '00', 12)
+    return morning_urls + evening_urls
+
+
+def get_image_urls(site, year, month, day, hour, minute, duration):
+    url = "http://climate.weather.gc.ca/radar/index_e.html?site=%(site)s&year=%(year)i&month=%(month)i&day=%(day)i&hour=%(hour)s&minute=%(minute)s&duration=%(duration)i&image_type=PRECIPET_RAIN_WEATHEROFFICE" % locals()
+    print url
+
+    html = urllib2.urlopen(url)
+    soup = BeautifulSoup(html)
+
+    script = soup.findAll('script')[2]
+
+    js = re.search(r'^\s*blobArray\s*=\s*(\[.*?\])\s*,\s*$', script.string, flags=re.DOTALL | re.MULTILINE).group(1).replace("'",'"')
+
+    # too dirty to parse with json
+    images = []
+    pattern = re.compile(r'(\/lib\/radar\/image\.php\?time=.*&site=[A-Z]*)')
+    for image in re.findall(pattern, js):
+        images.append(image)
+
+    return images[:12]
+
