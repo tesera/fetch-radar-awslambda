434821b3269aaa70e9f40d4c9d9a8f2f16c1b3d0
zmpeg <zandstra.matt@gmail.com>
2016-10-17 14:06:26 -0600

    (broken) fetch the images function with test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
+const http = require('http');
+const request = require('request');
+
 require('node-env-file')('.env');
+
 
 exports.handler = function(event, context) {
 
@@ -11,4 +15,23 @@ exports.handler = function(event, context) {
     console.log('Sites: ', sites);
     console.log('Types: ', types);
     console.log('Time:  ', event['time']);
+};
+
+exports.getImageURLs = function(site, type, datetime, cb) {
+    var duration = 12;
+
+    imageListURL = `http://climate.weather.gc.ca/radar/index_e.html?site=${site}&year=${datetime.getFullYear()}&month=${datetime.getMonth()}&day=${datetime.getDate()}&hour=${datetime.getHours()}&minute=${datetime.getMinutes()}&duration=${duration}&image_type=${type}`
+    request(imageListURL, (error, response, body) => {
+        if(!error && response.statusCode == 200) {
+            var re = /^\s*blobArray = \[([\s\S]*)\],$/gm;
+            var blobArray = re.exec(body)[1]
+                .split('\n')
+                .filter((s) => { return !s.match(/^\s+$/); })
+                .map((s) => { return /s*'(.*)',/.exec(s)[1]; });
+            cb(blobArray);
+        } else {
+            cb(null, response);
+        }
+    });
+    // console.log(site, type, datetime);
 };
---
+++ test/get_image_urls.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const lambda = require('../index');
+
+
+describe('getImageURLs()', () => {
+    var site = 'WUJ';
+    var type = 'PRECIPET_SNOW_WEATHEROFFICE'
+    var datetime = new Date(2015,10,17,10);
+
+    it('returns the correct data', (done) => {
+
+        var expected = [
+            '/lib/radar/image.php?time=17-OCT-15+10.10.35.793302+AM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+11.11.03.537530+AM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+12.10.49.841273+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+01.10.48.682039+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+02.10.55.858334+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+03.10.49.030733+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+04.10.35.781304+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+05.10.37.297283+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+06.10.39.525245+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+07.10.38.930713+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+08.10.34.513546+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+09.10.35.458651+PM&site=WUJ',
+            '/lib/radar/image.php?time=17-OCT-15+10.10.29.481384+PM&site=WUJ',
+        ];
+
+        lambda.getImageURLs(site, type, datetime, (actual, err) => {
+            if (err) done(err);
+            else
+            {
+                assert.deepEqual(expected, actual);
+            }
+        });
+    });
+});
