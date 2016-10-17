1a0999992ee7a570e8d9ff122f68cc0fae630dbd
zmpeg <zandstra.matt@gmail.com>
2016-10-17 11:46:37 -0600

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 [ ! -f .env ] && cp sample.env .env
 
-zip -r function.zip radar.py lib .env
+zip -r function.zip index.js .env node_modules
 
 aws lambda update-function-code \
 --function-name fetch-radar \
