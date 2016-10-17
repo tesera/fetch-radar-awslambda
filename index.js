7c14e4be58b9f0a0fe8133e253b74c30dc9619de
zmpeg <zandstra.matt@gmail.com>
2016-10-17 11:54:39 -0600

    add env file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 var AWS = require('aws-sdk');
+require('node-env-file')('.env');
 
 exports.handler = function(event, context) {
     console.log('Lambda Event: %j', event);
+    console.log('Sites: ', process.env.SITES);
+    console.log('Types: ', process.env.TYPES);
 };
