af64084391a2587247282678e3d79e33fbba4c8d
zmpeg <zandstra.matt@gmail.com>
2016-11-01 11:34:36 -0600

    make process_site test pass

--- test/process_site.js
+++ test/process_site.js
@@ -9,13 +9,20 @@ describe('processSite()', function() {
     var datetime = new Date(2015,10,17);
 
     it('Makes a call for each type 24x for a 24hr period', function(done) {
+        var expected = [
+            [ 'image array result', 'image array result' ],
+            [ 'image array result', 'image array result' ]
+        ];
         this.timeout(0);
 
+        lambda.getImageURLs = function() {
+            return "image array result";
+        };
+
         lambda.processSite(site, datetime).then((actual) => {
-            done("WIP");
+            assert.deepEqual(expected, actual);
+            done();
         })
         .catch(done);
-
-        var expected = false;
     });
 });
